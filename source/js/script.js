53657fd78fe6235d977b49d5208dbfd4234c5f49
Georgij Tsabiev <gtsabiev@inbox.ru>
2023-09-07 15:04:58 +0300

    Добавляет js файл и логику для открытия и закрытия навигации

---
+++ source/js/script.js
@@ -0,0 +1,15 @@
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
+let navMainWrapper = document.querySelector('.main-nav__wrapper')
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+    navMainWrapper.style.display = "block";
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+    navMainWrapper.style.display = "none";
+  }
+});
