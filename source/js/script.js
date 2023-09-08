e2ef33fd356d9920db7eb7a3cf84b2ef1a7f0f5e
Georgij Tsábiev <gtsabiev@inbox.ru>
2023-09-08 16:33:00 +0300

    Реализует открытое меню навигации без JS

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,7 @@
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
-let navMainWrapper = document.querySelector('.main-nav__wrapper')
+let navMainWrapper = document.querySelector('.main-nav__wrapper');
+
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
@@ -13,3 +14,16 @@ navToggle.addEventListener('click', function () {
     navMainWrapper.style.display = "none";
   }
 });
+
+let navNoJs = document.querySelector('.main-nav--nojs');
+let mainNavList = document.querySelector('.main-nav__list');
+
+if (navMain.classList.contains('main-nav--nojs')) {
+  navMainWrapper.style.display = "block";
+  navToggle.style.display = "none";
+  mainNavList.style.flexDirection = "row";
+  mainNavList.style.border = "none";
+  mainNavList.style.gap = "5px 25px";
+  mainNavList.style.flexWrap = "wrap";
+  mainNavList.style.paddingTop = "5px";
+}
